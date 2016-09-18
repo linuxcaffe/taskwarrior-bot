2ad69c35babc43e89869e64ac42e1a6e0a403251
German Lashevich <german.lashevich@tatar.ru>
2016-09-19 02:39:54 +0300

    Initial commit

---
+++ bot.py
@@ -0,0 +1,20 @@
+import sys
+
+from telegram.ext import Updater, CommandHandler
+
+
+def start(bot, update):
+    bot.sendMessage(update.message.chat_id, text='Hello World!')
+
+
+def hello(bot, update):
+    bot.sendMessage(update.message.chat_id,
+                    text='Hello {}'.format(update.message.from_user.first_name))
+
+updater = Updater(sys.argv[1])
+
+updater.dispatcher.add_handler(CommandHandler('start', start))
+updater.dispatcher.add_handler(CommandHandler('hello', hello))
+
+updater.start_polling()
+updater.idle()
